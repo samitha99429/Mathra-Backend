aed8d0e6af7e2eb2b38c33e2e694da0a1f08408e
Madhusha Prasad <50085447+MadhushaPrasad@users.noreply.github.com>
2022-11-07 08:25:19 +0530

    vaccine description model

---
+++ models/VaccineDescription.model.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const vaccineDescriptionSchema = new Schema({
+  descriptionID: { type: String, required: true },
+  vaccineName: { type: String, required: true },
+  vaccineDetails: { type: String, required: true },
+});
+
+const VaccineDescriptionModel = mongoose.model(
+  'VaccineModel',
+  vaccineDescriptionSchema
+);
+module.exports = VaccineDescriptionModel;
--- models/VaccineSchedule.model.js
+++ models/VaccineSchedule.model.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const vaccineSchema = new Schema({
+const vaccineScheduleSchema = new Schema({
   vaccineID: { type: String, required: true },
   vaccineName: { type: String, required: true },
   vaccineDate: { type: String, required: true },
   vaccineTime: { type: String, required: true },
 });
 
-const VaccineModel = mongoose.model('VaccineModel', vaccineSchema);
+const VaccineModel = mongoose.model('VaccineModel', vaccineScheduleSchema);
 module.exports = VaccineModel;
